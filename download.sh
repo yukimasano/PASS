a287dd3b507acd510ea6e570fa6e73f0968bb66d
Yuki M. Asano <yuki.m.asano@gmail.com>
2021-09-24 11:07:20 +0100

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# download files
+for PART in 1 2 3 4 5 6 7 8 9
+do
+   curl  https://zenodo.org/record/5501843/files/PASS.tar.part.${PART} --output PASS.tar.part.${PART}
+done
+
+# concatenate parts
+cat PASS.tar.part* > PASS.tar
+
+# extract dataset
+## will create dataset with images in PASS/dummy_folder/img-hash.jpeg
+tar -xf PASS.tar
+
+# you can use this now e.g. with torchvision.datasets.ImageFolder('/dir/to/PASS')
