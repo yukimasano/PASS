bafe3bdf0e030f3925d882dad01f5440c624d164
Yuki M. Asano <yuki.m.asano@gmail.com>
2021-09-26 10:38:54 +0100

    updated for the new individually usable tars ver.

--- download.sh
+++ download.sh
@@ -2,17 +2,14 @@
 
 # download files
 echo "downloading dataset tar files"
-for PART in 1 2 3 4 5 6 7 8 9
+for PART in 0 1 2 3 4 5 6 7 8 9
 do
    echo "download part" $PART
-   curl  https://zenodo.org/record/5501843/files/PASS.tar.part.${PART} --output PASS.tar.part.${PART}
+   curl  https://zenodo.org/record/5528345/files/PASS.${PART}.tar --output PASS.${PART}.tar
 done
 
-# concatenate parts
-cat PASS.tar.part* > PASS.tar
-
 # extract dataset
-## will create dataset with images in PASS/dummy_folder/img-hash.jpeg
-tar -xf PASS.tar
+## will create dataset with images in PASS_dataset/dummy_folder/img-hash.jpeg
+for file in *.tar; do tar -xf "$file"; done
 
 # you can use this now e.g. with torchvision.datasets.ImageFolder('/dir/to/PASS')
